0d875d2ed93614b87f895d7f96c9ccde71aff2ed
SRenuuu <sandulrenuja@gmail.com>
2024-02-24 00:21:44 +0530

    Add support for sass and update styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
+  sassOptions: {
+    prependData: `@import "./_mantine.scss";`,
+  },
+};
 
 export default nextConfig;
