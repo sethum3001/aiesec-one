fb131c536574452dafd0ff5e5abfde3f424abbd9
SRenuuu <sandulrenuja@gmail.com>
2024-02-24 01:26:03 +0530

    Add eslint, prettier and setup husky

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  experimental: {
+    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
 };
 
