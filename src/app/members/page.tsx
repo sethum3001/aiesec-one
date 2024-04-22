43ca6a7a36f0731d6316359183316889aa9025dd
_im'kavindu <65580465+KavinduUoM20@users.noreply.github.com>
2024-04-23 04:44:47 +0530

    Member Form Added

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,86 +1,279 @@
 "use client";
 
-import React from "react";
-import Papa from "papaparse";
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "mantine-react-table/styles.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useMemo, useState } from "react";
+import {
+  MRT_EditActionButtons,
+  MantineReactTable,
+  type MRT_ColumnDef,
+  type MRT_Row,
+  type MRT_TableOptions,
+  useMantineReactTable
+} from "mantine-react-table";
+import {
+  ActionIcon,
+  Button,
+  Flex,
+  Stack,
+  Text,
+  Title,
+  Tooltip,
+  Box
+} from "@mantine/core";
+import { modals } from "@mantine/modals";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import classes from "./members.module.scss";
+import { ResourceResponse } from "@/app/types/ResourceResponse";
+import {
+  useCreateResource,
+  useDeleteResource,
+  useGetResources,
+  useUpdateResource
+} from "@/app/hooks/resources";
+import { validateRequired, validateUrl } from "@/app/util/dataUtils";
 
-import { useEffect, useState } from "react";
-
-const acceptableFileTypes =
-  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel, .csv";
-
-const Members: React.FC = () => {
-  const [message, setMessage] = useState("");
-
-  // Function to send API request with EXPA IDs
-  const sendAPIRequest = async (expaIds: string[]) => {
-    try {
-      const response = await fetch("/api/members", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ expaIds })
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        console.log("API response:", data);
-        // Handle API response as needed
-      } else {
-        throw new Error("Failed to send API request: " + response.status);
-      }
-    } catch (error) {
-      console.error("API request failed:", error);
-      // Handle error gracefully
-    }
+function validateResource(resource: ResourceResponse) {
+  return {
+    title: !validateRequired(resource.title) ? "Title is Required" : "",
+    url: !validateUrl(resource.url) ? "Invalid URL Format" : "",
+    // description: !validateRequired(resource.description) ? "Description is Required" : "",
+    link: !validateUrl(resource.link) ? "Invalid Link Format" : "",
+    // functions: !validateRequired(resource.functions) ? "Functions are Required" : "",
+    keywords: !validateRequired(resource.keywords)
+      ? "Keywords are Required"
+      : ""
   };
+}
 
-  //Parse CSV
-  const parseCSVFile = (file: File) => {
-    Papa.parse(file, {
-      skipEmptyLines: true,
-      header: true,
-      error: function (error) {
-        console.error("CSV parsing error:", error);
+const MembersPage = () => {
+  const [validationErrors, setValidationErrors] = useState<
+    Record<string, string | undefined>
+  >({});
+
+  const columns = useMemo<MRT_ColumnDef<ResourceResponse>[]>(
+    () => [
+      {
+        accessorKey: "name",
+        header: "Name"
       },
-
-      complete: function (results) {
-        console.log("Finished parsing CSV:", results.data);
-        const expaIds = results.data
-          .map((row) => row.expa_id)
-          .filter((id) => id);
-        console.log("EXPA IDs extracted from CSV:", expaIds);
-
-        // Send API request with extracted EXPA IDs
-        if (expaIds.length > 0) {
-          sendAPIRequest(expaIds);
-          console.log("send request worked");
-        } else {
-          console.warn("No EXPA IDs found in CSV");
-        }
+      {
+        accessorKey: "role",
+        header: "Role"
+      },
+      {
+        accessorKey: "entity",
+        header: "Entity"
+      },
+      {
+        accessorKey: "email",
+        header: "Email"
+      },
+      {
+        accessorKey: "expa_id",
+        header: "Expa ID"
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+        Cell: ({ cell }) => (
+          <div
+            style={{
+              height: "10px",
+              width: "10px",
+              backgroundColor: cell.getValue() === "Active" ? "green" : "red",
+              borderRadius: "50%",
+              display: "inline-block"
+            }}
+          />
+        )
+      },
+      {
+        accessorKey: "view",
+        header: "View",
+        Cell: () => <i className="bi-eye"></i> // Use the correct class for the eye icon
       }
-    });
-  };
+    ],
+    [validationErrors]
+  );
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      parseCSVFile(file);
+  // Custom hooks for CRUD operations
+  const { mutateAsync: createResource, isLoading: isCreatingResource } =
+    useCreateResource();
+  const {
+    data: fetchedResources = [],
+    isError: isLoadingResourcesError,
+    isFetching: isFetchingResources,
+    isLoading: isLoadingResources
+  } = useGetResources();
+  const { mutateAsync: updateResource, isLoading: isUpdatingResource } =
+    useUpdateResource();
+  const { mutateAsync: deleteResource, isLoading: isDeletingResource } =
+    useDeleteResource();
+
+  // Handlers for CRUD operations
+  const handleCreateResource: MRT_TableOptions<ResourceResponse>["onCreatingRowSave"] =
+    async ({ values, exitCreatingMode }) => {
+      const newValidationErrors = validateResource(values);
+      if (Object.values(newValidationErrors).some((error) => error)) {
+        setValidationErrors(newValidationErrors);
+        return;
+      }
+      setValidationErrors({});
+      await createResource(values);
+      exitCreatingMode();
+    };
+
+  const handleEditResource: MRT_TableOptions<ResourceResponse>["onEditingRowSave"] =
+    async ({ row, values, table }) => {
+      const newValidationErrors = validateResource(values);
+      if (Object.values(newValidationErrors).some((error) => error)) {
+        setValidationErrors(newValidationErrors);
+        return;
+      }
+      setValidationErrors({});
+      console.log(values);
+      await updateResource({ ...values, _id: row.original._id });
+      table.setEditingRow(null); //exit editing mode
+    };
+
+  const openDeleteConfirmModal = (row: MRT_Row<ResourceResponse>) =>
+    modals.openConfirmModal({
+      title: "Confirmation",
+      children: (
+        <Text>
+          Are you sure you want to delete this record? This action cannot be
+          undone.
+        </Text>
+      ),
+      labels: { confirm: "Delete", cancel: "Cancel" },
+      confirmProps: { color: "red" },
+      onConfirm: () => deleteResource(row.original._id)
+    });
+
+  const table = useMantineReactTable({
+    columns,
+    data: fetchedResources,
+    createDisplayMode: "modal", //default ('row', and 'custom' are also available)
+    editDisplayMode: "modal", //default ('row', 'cell', 'table', and 'custom' are also available)
+    enableEditing: true,
+    getRowId: (row) => row._id,
+    mantineToolbarAlertBannerProps: isLoadingResourcesError
+      ? {
+          color: "red",
+          children: "Error loading data"
+        }
+      : undefined,
+    mantineTableContainerProps: {
+      style: {
+        minHeight: "500px"
+      }
+    },
+    onCreatingRowCancel: () => setValidationErrors({}),
+    onCreatingRowSave: handleCreateResource,
+    onEditingRowCancel: () => setValidationErrors({}),
+    onEditingRowSave: handleEditResource,
+    positionActionsColumn: "last",
+    renderCreateRowModalContent: ({ table, row, internalEditComponents }) => (
+      <Stack>
+        <Title order={3}>Create Resource</Title>
+        {internalEditComponents}
+        <Flex justify="flex-end" mt="xl">
+          <MRT_EditActionButtons variant="text" table={table} row={row} />
+        </Flex>
+      </Stack>
+    ),
+    renderEditRowModalContent: ({ table, row, internalEditComponents }) => (
+      <Stack>
+        <Title order={3}>Edit Resource</Title>
+        {internalEditComponents}
+        <Flex justify="flex-end" mt="xl">
+          <MRT_EditActionButtons variant="text" table={table} row={row} />
+        </Flex>
+      </Stack>
+    ),
+    renderRowActions: ({ row, table }) => (
+      <Flex gap="md">
+        <Tooltip label="Edit">
+          <ActionIcon
+            variant="subtle"
+            size="sm"
+            onClick={() => table.setEditingRow(row)}
+          >
+            <IconEdit />
+          </ActionIcon>
+        </Tooltip>
+        <Tooltip label="Delete">
+          <ActionIcon
+            variant="subtle"
+            size="sm"
+            color="red"
+            onClick={() => openDeleteConfirmModal(row)}
+          >
+            <IconTrash />
+          </ActionIcon>
+        </Tooltip>
+      </Flex>
+    ),
+    state: {
+      isLoading: isLoadingResources,
+      isSaving: isCreatingResource || isUpdatingResource || isDeletingResource,
+      showAlertBanner: isLoadingResourcesError,
+      showProgressBars: isFetchingResources
     }
-  };
+  });
 
   return (
-    <div>
-      <h1>Upload CSV File</h1>
-      <input
-        type="file"
-        name="file"
-        accept={acceptableFileTypes}
-        onChange={handleFileChange}
-      />
-      <p>Supported file types: CSV, Excel (.xlsx, .xls)</p>
+    <div className={classes.body}>
+      <Title mt={8} mb={24} order={1} style={{ color: "#1C7ED6" }}>
+        Members
+      </Title>
+      <Box
+        my={20}
+        style={{
+          display: "flex",
+          flexDirection: "row"
+        }}
+      >
+        <div style={{ position: "relative" }}>
+          <input
+            type="text"
+            placeholder="Search Members.."
+            style={{ paddingLeft: "55px", width: "619px", height: "48px" }}
+          />
+          <i
+            className="bi bi-search"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "25px",
+              transform: "translateY(-50%)"
+            }}
+          ></i>
+        </div>
+        <Button style={{ height: "48px", left: "35px" }}>
+          <i
+            className="bi bi-person-plus-fill"
+            style={{ marginRight: "5px" }}
+          ></i>{" "}
+          Import Members
+        </Button>
+        <Button style={{ height: "48px", left: "55px" }}>
+          <i className="bi bi-key-fill" style={{ marginRight: "5px" }}></i>
+          Access Control
+        </Button>
+        <Button style={{ height: "48px", left: "75px" }}>
+          <i className="bi bi-people-fill" style={{ marginRight: "5px" }}></i>
+          User Groups
+        </Button>
+      </Box>
+      <div>
+        <MantineReactTable table={table} />
+      </div>
     </div>
   );
 };
 
-export default Members;
+export default MembersPage;
